cab2de81f8e0d34449a907782eda94c51acc62fc
Stefano <piero.quiroz93@gmail.com>
2021-05-10 17:26:35 -0500

    Consumiendo datos de una API con fetch()

---
+++ API/README.md
@@ -0,0 +1,3 @@
+# Fetch
+
+Consumiendo datos de la API https://pokeapi.co/api/v2/pokemon/1/ con fetch(my request) y response.json() en un script de html
---
+++ API/consumiendoAPI.js
@@ -0,0 +1,13 @@
+/* fetch("http://www.example.com")
+    .then(response => console.log(response))
+    .catch(err => console.log(err)); */
+
+//Usando async await
+
+/* const funcion = async () => {
+    let response = await fetch ("https://www.ford.com/");
+    console.log(response);
+}
+
+funcion(); */
+
