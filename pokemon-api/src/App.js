74904c94eaf0f27a3b9b1d9239699cff8072f362
Stefano <piero.quiroz93@gmail.com>
2021-05-11 12:04:23 -0500

    Pokemon-Api Own Version 5%

---
+++ pokemon-api/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.css';
+import PokemonApi from './components/PokemonApi'
+function App() {
+  return (
+    <div className="App">
+      <PokemonApi/>
+    </div>
+  );
+}
+
+export default App;
---
+++ pokemon-api/src/components/PokemonApi.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PokemonApi = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default PokemonApi;
