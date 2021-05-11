570f19edd3b3fb4b3585243158243e6f4b979870
Stefano <piero.quiroz93@gmail.com>
2021-05-11 18:10:27 -0500

    Pokemon - API Ayudantía

--- pokemon-api/src/components/PokemonApi.js
+++ pokemon-api/src/components/PokemonApi.js
@@ -28,20 +28,51 @@ const StyledDiv = styled.div`
 
 const PokemonApi = (props) => {
     const [state, setState] = useState([]);
-    
-    const onClickHandle = () => {
+    //Method onChange
+    const onChangeClick = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setState({
+            ...state,
+            [name]:value,
+        })
+        
+    }
+
+    //Method onClick
+    const onClickHandle = (e) => {
+        e.preventDefault();
         fetch("https://pokeapi.co/api/v2/pokemon/?limit=807")
             .then(response => response.json())
             .then(response => setState(response.results));
     }
 
+    /* const findPokemon =()=>{
+        for(let i=0; i<state.length; i++){
+            if(state.name === state[i].name)
+        }
+    } */
+    //list-items
     const items = state.length>0 && state.map((pokemon, index) => (<StyledDiv key={index}>{pokemon.name}</StyledDiv>));
     return (
         <div>
-            <StyledButton onClick={onClickHandle}>Fetch Pokemons</StyledButton>
+            <div>
+                <label htmlFor="pokemonName">Buscar Pokemon: </label>
+                <input type="text" name="pokemonName" placeholder="Ingrese Pokemon" onChange={onChangeClick}/>
+                {/* {
+                    state.map(p=>{ (p.name === state.name) ?
+                        <h3>Existe</h3> : 
+                        <h3>No existe</h3>
+                    })
+                } */}
+            </div>
+            <StyledButton onClick={onClickHandle} value={state}>Fetch Pokemons</StyledButton>
             <div>{items}</div>
         </div>
     );
 }
 
 export default PokemonApi;
+
+//onClick cuando aparezca y desaparezca
+//cuando recibo un input y quiero comprarlo con el item,name de pokemon
